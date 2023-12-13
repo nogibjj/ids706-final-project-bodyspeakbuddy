b1cbbbd236bf5d63da746d7210cf1bfc36872518
Nastiiasaenko <54864655+Nastiiasaenko@users.noreply.github.com>
2023-12-12 21:49:44 -0500

    Update and rename test_app.py to test_main.py

--- test_app.py
+++
@@ -1,6 +0,0 @@
-def add_smth(a,b):
-  return a+b
-
-if __name__ == "__app__":
-    add_smth()
-    print("All tests passed.")
---
+++ test_main.py
@@ -0,0 +1,5 @@
+def add_smth(a,b):
+  return a+b
+
+add_smth(1,2)
+print("All tests passed.")
