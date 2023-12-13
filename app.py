9ccc4e211297fa7a9b53953da63bee1dc424f3b3
Gunel Aghakishiyeva <78721466+aghakishiyeva@users.noreply.github.com>
2023-12-12 19:15:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -190,7 +190,7 @@ print(data.head())
 # ------
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="192.168.1.138", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
 
 
 # How does crossing one's arms typically affect the perception of their attitude in a conversation?
